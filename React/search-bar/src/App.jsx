d81fa3ce964b2489927914bbba1b792e26425265
Huakun Shen <shenhuakun@outlook.com>
2020-04-08 17:05:14 -0400

    fixed submodule problem

---
+++ React/search-bar/src/App.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useRef, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+const SearchbarDropdown = (props) => {
+  const { options, onInputChange } = props;
+  const ulRef = useRef();
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.addEventListener('click', (event) => {
+      event.stopPropagation();
+      ulRef.current.style.display = 'flex';
+      onInputChange(event);
+    });
+    document.addEventListener('click', (event) => {
+      ulRef.current.style.display = 'none';
+    });
+  }, []);
+  return (
+    <div className="search-bar-dropdown">
+      <input
+        id="search-bar"
+        type="text"
+        className="form-control"
+        placeholder="Search"
+        ref={inputRef}
+        onChange={onInputChange}
+      />
+      <ul id="results" className="list-group" ref={ulRef}>
+        {options.map((option, index) => {
+          return (
+            <button
+              type="button"
+              key={index}
+              onClick={(e) => {
+                inputRef.current.value = option;
+              }}
+              className="list-group-item list-group-item-action"
+            >
+              {option}
+            </button>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+const defaultOptions = [];
+for (let i = 0; i < 10; i++) {
+  defaultOptions.push(`option ${i}`);
+  defaultOptions.push(`suggestion ${i}`);
+  defaultOptions.push(`advice ${i}`);
+}
+
+function App() {
+  const [options, setOptions] = useState([]);
+  const onInputChange = (event) => {
+    setOptions(
+      defaultOptions.filter((option) => option.includes(event.target.value))
+    );
+  };
+
+  return (
+    <div className="App container mt-2 mb-3">
+      <h1>Search Bar Dropdown</h1>
+      <SearchbarDropdown options={options} onInputChange={onInputChange} />
+      <br />
+      <button className="btn btn-primary">Search</button>
+    </div>
+  );
+}
+
+export default App;
